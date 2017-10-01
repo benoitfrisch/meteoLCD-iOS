4935078ca523f7d95f81a2eb3aa1965e0f6b71b3
Benoît Frisch <benoit@fresh.lu>
2017-10-02 00:44:08 +0200

    Update License in all Documents

--- meteoLCDTests/meteoLCDTests.swift
+++ meteoLCDTests/meteoLCDTests.swift
@@ -1,10 +1,21 @@
-//
-//  meteoLCDTests.swift
-//  meteoLCDTests
-//
-//  Created by Benoît Frisch on 30/09/2017.
-//  Copyright © 2017 Benoît Frisch. All rights reserved.
-//
+/**
+ * This file is part of meteoLCD.
+ *
+ * Copyright (c) 2017 Benoît FRISCH
+ *
+ * meteoLCD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * meteoLCD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with meteoLCD If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import XCTest
 @testable import meteoLCD
